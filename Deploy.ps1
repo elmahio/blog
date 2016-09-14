1810b986e0d3711a61103ec5e3718842cbe13bac
Thomas Ardal <thomasardal@gmail.com>
2016-09-14 07:10:47 +0200

    Added deploy script

---
+++ Deploy.ps1
@@ -0,0 +1,8 @@
+﻿mkdocs build --clean
+git checkout gh-pages
+Copy-Item -Path .\site\* -Destination . -Recurse -Force
+Remove-Item -Path .\site\ -Recurse -Force
+git add .
+git commit -m "Build newest version"
+git push
+git checkout master
