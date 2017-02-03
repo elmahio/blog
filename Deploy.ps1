1451ceb27894c6c93a3ed10ec81022daa4350443
Thomas Ardal <thomasardal@gmail.com>
2017-02-03 15:10:11 +0100

    .htaccess not supported on github pages

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,5 +1,4 @@
 ﻿mkdocs build --clean
-Rename-Item .\site\htaccess .htaccess
 git checkout gh-pages
 Copy-Item -Path .\site\* -Destination . -Recurse -Force
 Remove-Item -Path .\site\ -Recurse -Force
