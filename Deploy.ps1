8e54c74ed7191a5e1e488e9d7963000acb91a758
Thomas Ardal <thomasardal@gmail.com>
2017-02-03 15:04:29 +0100

    Added htaccess to redirect https to http (for now)

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,4 +1,5 @@
 ﻿mkdocs build --clean
+Rename-Item .\site\htaccess .htaccess
 git checkout gh-pages
 Copy-Item -Path .\site\* -Destination . -Recurse -Force
 Remove-Item -Path .\site\ -Recurse -Force
